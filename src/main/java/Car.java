9cd46e2bb5a0829715e978c8e01a66bc59ca3733
jaferrizvi <jafershah34@gmail.com>
2021-01-11 20:29:38 -0800

    Update Car.java
    added more constructors

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,5 +1,5 @@
 public class Car {
-  int modelYear;
+  int modelYear = 999;
   String modelName;
     
   public Car(int year, String name) {
@@ -9,26 +9,41 @@ public class Car {
   }
   public Car(String name) {
 	    modelName = name;
-	    System.out.println("I m in object myCar");
+	    System.out.println("I m in object myCar3");
   }
 
      public static void main(String[] args) {
 	 
     Car myCar = new Car(1969, "Mustang");
+    myCar.modelYear = 2021;
+    Car myCar4 = new Car(1969, "Mustang");
+    myCar4.modelName = "Pacifica";
    //System.out.println(modelYear + " " + modelName);
-   // Car mycar2 = new Car();
+    Car mycar2 = new Car();
     myCar.printInstanceValues();
-   // mycar2.printInstanceValues();
+    mycar2.printInstanceValues();
+    Car myCar3 = new Car("Volvo");
+    myCar3.printInstanceValues();
+    myCar4.printInstanceValues();
+    
   }
+     
      public void printInstanceValues() {
   
 	  System.out.println(modelYear + " " + modelName);
   }
-	/*
-	 * public Car() { modelYear = 2021; modelName = "Honda"; }
-	 */
+
+	 public Car()
+	 { 
+		 modelYear = 2020;
+	 	 modelName = "Honda";
+	 	 System.out.println("I m in object myCar2");
+	 }
+	 
   // public Car() {
     	// System.out.println("I m in object myCar2");
+     
+     
      }
 
 // Outputs 1969 Mustang
