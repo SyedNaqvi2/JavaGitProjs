d66b332549f74f34928c0854e738775c80aa580c
jaferrizvi <jafershah34@gmail.com>
2021-01-11 19:08:06 -0800

    Create Car.java

---
+++ src/main/java/Car.java
@@ -0,0 +1,32 @@
+public class Car {
+  int modelYear;
+  String modelName;
+    
+  public Car(int year, String name) {
+    modelYear = year;
+    modelName = name;
+    System.out.println("I m in object myCar");
+  }
+
+     public static void main(String[] args) {
+	 
+    Car myCar = new Car(1969, "Mustang");
+   //System.out.println(modelYear + " " + modelName);
+   // Car mycar2 = new Car();
+    myCar.printInstanceValues();
+   // mycar2.printInstanceValues();
+  }
+     public void printInstanceValues() {
+  
+	  System.out.println(modelYear + " " + modelName);
+  }
+	/*
+	 * public Car() { modelYear = 2021; modelName = "Honda"; }
+	 */
+  // public Car() {
+    	// System.out.println("I m in object myCar2");
+     }
+
+// Outputs 1969 Mustang
+
+
